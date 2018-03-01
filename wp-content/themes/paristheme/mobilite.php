2c3b52b379ac008ee20caaf4a80145f53a122213
nbrion <n.brion41@gmail.com>
2018-03-01 10:45:54 +0100

    Template mobilite

---
+++ wp-content/themes/paristheme/mobilite.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Template Name: mobilite
+ */
+
+get_header(); ?>
+
+	<section id="primary" class="content-area col-sm-12">
+		<main id="main" class="site-main" role="main">
+
+			enfnkdsjfdsjkncdsjknc
+
+		</main><!-- #main -->
+	</section><!-- #primary -->
+
+<?php
+get_footer();
